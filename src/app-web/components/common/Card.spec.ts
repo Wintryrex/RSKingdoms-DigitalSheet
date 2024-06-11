2ce0a2d860596085e02b4f3a86a2fb9fb93e7d17
Awni <Wintrygamer@gmail.com>
2024-06-11 22:58:58 +0200

    Move username to general card component and add card for location

---
+++ src/app-web/components/common/Card.spec.ts
@@ -0,0 +1,14 @@
+﻿import { it, expect } from 'vitest'
+import { mount } from '@vue/test-utils'
+import Card from '../common/Card.vue'
+
+it('renders component with the right label', () => {
+  const wrapper = mount(Card, {
+    props: {
+      title: 'Name',
+      placeholder: 'Username',
+      ariaLabel: 'Username'
+    }
+  })
+  expect(wrapper.text()).toContain('Name')
+})
--- src/app-web/views/HomeView.vue
+++ src/app-web/views/HomeView.vue
@@ -1,11 +1,14 @@
 <script setup lang="ts">
-import Username from '../components/username/Username.vue'
+import Card from '../components/common/Card.vue'
 </script>
 
 <template>
   <main class="row">
-    <div class="col-6">
-      <Username />
+    <div class="col-5">
+      <Card title="Name" placeholder="Username" ariaLabel="Username" />
+    </div>
+    <div class="col-5">
+      <Card title="Last Location" placeholder="Location" ariaLabel="Location" />
     </div>
   </main>
 </template>
