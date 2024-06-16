fc0afc7a5280ea3531aa7faa55c2a58dd1c60083
Awni <Wintrygamer@gmail.com>
2024-06-16 21:53:37 +0200

    Change to use of card component

--- src/app-web/components/skills/Skills.vue
+++ src/app-web/components/skills/Skills.vue
@@ -1,21 +1,19 @@
 ﻿<script setup lang="ts">
 import Skill from './Skill.vue'
+import Card from '../common/Card.vue'
 </script>
 
 <template>
-  <div class="card">
-    <div class="card-header">Skills</div>
-    <div class="card-body">
-      <Skill :class="'skill'" title="Attack" src="/attack.svg" alt="Attack Skill"></Skill>
-      <Skill :class="'skill'" title="Ranged" src="/range.svg" alt="Range Skill"></Skill>
-      <Skill :class="'skill'" title="Magic" src="/magic.svg" alt="Magic Skill"></Skill>
-      <Skill :class="'skill'" title="Defence" src="/defense.svg" alt="Defence Skill"></Skill>
-      <Skill :class="'skill'" title="Thieving" src="/thieving.svg" alt="Thieving Skill"></Skill>
-      <Skill :class="'skill'" title="Gathering" src="/gathering.svg" alt="Gathering Skill"></Skill>
-      <Skill :class="'skill'" title="Crafting" src="/crafting.svg" alt="Crafting Skill"></Skill>
-      <Skill :class="'skill'" title="Cooking" src="/cooking.svg" alt="Cooking Skill"></Skill>
-    </div>
-  </div>
+  <Card title="Skills">
+    <Skill :class="'skill'" title="Attack" src="/attack.svg" alt="Attack Skill"></Skill>
+    <Skill :class="'skill'" title="Ranged" src="/range.svg" alt="Range Skill"></Skill>
+    <Skill :class="'skill'" title="Magic" src="/magic.svg" alt="Magic Skill"></Skill>
+    <Skill :class="'skill'" title="Defence" src="/defense.svg" alt="Defence Skill"></Skill>
+    <Skill :class="'skill'" title="Thieving" src="/thieving.svg" alt="Thieving Skill"></Skill>
+    <Skill :class="'skill'" title="Gathering" src="/gathering.svg" alt="Gathering Skill"></Skill>
+    <Skill :class="'skill'" title="Crafting" src="/crafting.svg" alt="Crafting Skill"></Skill>
+    <Skill :class="'skill'" title="Cooking" src="/cooking.svg" alt="Cooking Skill"></Skill>
+  </Card>
 </template>
 
 <style>
