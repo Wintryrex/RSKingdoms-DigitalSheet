80dc59a4ceca772b834803f90836c9159f389a31
Awni <Wintrygamer@gmail.com>
2024-06-25 23:21:57 +0200

    Add wounds and tally component and improve import of modules

---
+++ src/app-web/stores/characterStore.ts
@@ -0,0 +1,14 @@
+﻿import { defineStore } from 'pinia'
+import { ref, computed } from 'vue'
+
+export const useCharacterStore = defineStore('character', () => {
+  const healthPoints = ref(3)
+  const increaseHealth = () => {
+    healthPoints.value++
+  }
+  const decreaseHealth = () => {
+    healthPoints.value--
+  }
+
+  return { healthPoints, increaseHealth, decreaseHealth }
+})
--- src/app-web/views/HomeView.vue
+++ src/app-web/views/HomeView.vue
@@ -1,13 +1,14 @@
 <script setup lang="ts">
 import Card from '../components/common/CardPanel.vue'
 import Skills from '../components/skills/SkillsList.vue'
+import WoundsList from '../components/health/wounds/WoundsList.vue'
 </script>
 
 <template>
   <main class="row">
-    <div class="col-6">
+    <div class="col-lg-6">
       <div class="row">
-        <div class="col-6">
+        <div class="col-lg-6">
           <Card title="Name">
             <input
               type="text"
@@ -17,7 +18,7 @@ import Skills from '../components/skills/SkillsList.vue'
             />
           </Card>
         </div>
-        <div class="col-6">
+        <div class="col-lg-6">
           <Card title="Last Location">
             <input
               type="text"
@@ -27,10 +28,22 @@ import Skills from '../components/skills/SkillsList.vue'
             />
           </Card>
         </div>
-        <!-- Left Elements -->
+        <div class="col-lg-6 mt-4">
+          <WoundsList />
+        </div>
+        <div class="col-lg-6 mt-4">
+          <Card title="Death Tally">
+            <input
+              type="text"
+              class="form-control rs-input"
+              placeholder="Amount"
+              aria-label="Death Amount"
+            />
+          </Card>
+        </div>
       </div>
     </div>
-    <div class="col-6">
+    <div class="col-lg-6">
       <Skills />
     </div>
   </main>
